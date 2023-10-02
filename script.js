3cc70d08dfd2e21d66c2156f2c7438cf4ab4045e
ANUS-CHOUHAN <121302224+ANUS-CHOUHAN@users.noreply.github.com>
2023-10-02 19:53:09 +0500

    js Second class

---
+++ script.js
@@ -0,0 +1,50 @@
+// num1 = prompt("Enter First number");
+// num2 = prompt("Enter Second number");
+// sum = num1/num2
+// console.log(sum);
+
+//v=s/t
+// f=ma
+// w=mg
+// a=v/t
+// w=fd
+
+//v=s/t
+// distance = prompt("Enter distance covered");
+// time = prompt("Enter time taken");
+// velocity = distance/time
+// document.write("The Velocity is ","",velocity,"m/s");
+
+//f=ma
+// mass = prompt("Enter mass");
+// accel = prompt("Enter acceleration");
+// force = mass*accel;
+// document.write("The force is"," ",force,"N");
+
+//addition
+// num1 = parseInt(prompt("Enter first num"));
+// num2 = parseInt(prompt("Enter Second num"));
+// sum= num1 + num2;
+// console.log(sum);
+// a= 2;
+// b = parseInt(prompt("Enter sec num"));
+
+// console.log(typeof(a));
+// console.log(typeof(b));
+
+
+
+
+
+// price = parseInt(prompt("Please select price of product 1)200 2)300 3)500"));
+// tax = (17/100)*price;
+// discount = (7/100)*price;
+// netAmount = ( price + tax )- discount;
+// document.write(netAmount);
+
+
+// price = parseInt(prompt("Please select price of product 1)200 2)300 3)500"));
+// tax = 0.17*price;
+// discount = 0.2*price;
+// netAmount = ( price + tax )- discount;
+// document.write(netAmount);
